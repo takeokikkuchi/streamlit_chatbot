25e13affe330018e18e19f9818d65b40a2da83dc
takeokikkuchi <96605483+takeokikkuchi@users.noreply.github.com>
2024-05-26 18:28:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,37 @@
 import streamlit as st
-from transformers import pipeline
 
-# Load the chatbot model
-# chatbot = pipeline("conversational", model="facebook/blenderbot-400M-distill")
-chatbot = pipeline(model="facebook/blenderbot-400M-distill")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+# import streamlit as st
+# from transformers import pipeline
 
-# Define the Streamlit app
-def main():
-    st.title("Chatbot Demo")
-    st.write("Welcome to the chatbot demo! Type your message below.")
+# # Load the chatbot model
+# # chatbot = pipeline("conversational", model="facebook/blenderbot-400M-distill")
+# chatbot = pipeline(model="facebook/blenderbot-400M-distill")
 
-    # User input text box
-    user_input = st.chat_input("You:")
-    if user_input:
-        st.write(f"User has sent the following prompt: {user_input}")
+# # Define the Streamlit app
+# def main():
+#     st.title("Chatbot Demo")
+#     st.write("Welcome to the chatbot demo! Type your message below.")
 
-    # if st.button("Send"):
-    #     if user_input.strip() != "":
-    #         try:
-    #             # Create a conversation object
-    #             conversation = Conversation(user_input)
-    #             # Get chatbot response
-    #             bot_response = chatbot(conversation)
-    #             response_text = bot_response.generated_responses[0]
-    #             st.text_area("Chatbot:", value=response_text, height=200)
-    #         except Exception as e:
-    #             st.error(f"An error occurred: {e}")
+#     # User input text box
+#     user_input = st.chat_input("You:")
+#     if user_input:
+#         st.write(f"User has sent the following prompt: {user_input}")
 
-# Run the app
-if __name__ == "__main__":
-    main()
+#     # if st.button("Send"):
+#     #     if user_input.strip() != "":
+#     #         try:
+#     #             # Create a conversation object
+#     #             conversation = Conversation(user_input)
+#     #             # Get chatbot response
+#     #             bot_response = chatbot(conversation)
+#     #             response_text = bot_response.generated_responses[0]
+#     #             st.text_area("Chatbot:", value=response_text, height=200)
+#     #         except Exception as e:
+#     #             st.error(f"An error occurred: {e}")
+
+# # Run the app
+# if __name__ == "__main__":
+#     main()
